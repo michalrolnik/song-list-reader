47667f4dd15a7163e11fdb4dc459ddb51c5fda1b
Michal <rolnikmichal@gmail.com>
2025-10-19 17:55:06 +0300

    clean backend: remove unused files and update module structure

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { SongsModule } from './songs/songs.module';
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule,SongsModule],
-  controllers: [AppController],
-  providers: [AppService],
+ 
 
 })
 export class AppModule {}
